a177eb7e8610d12a37096b20c23e57520912f140
Jessica Peruggia <jessica.peruggia@hotmail.com>
2021-04-01 14:44:18 -0300

    creating search filter

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -19,12 +19,35 @@ namespace MvcMovie.Controllers
             _context = context;
         }
 
-        // GET: Movies
-        public async Task<IActionResult> Index()
+    // GET: Movies
+    public async Task<IActionResult> Index(string movieGenre, string searchString)
+    {
+        // Use LINQ to get list of genres.
+        IQueryable<string> genreQuery = from m in _context.Movie
+                                        orderby m.Genre
+                                        select m.Genre;
+
+        var movies = from m in _context.Movie
+                    select m;
+
+        if (!string.IsNullOrEmpty(searchString))
         {
-            return View(await _context.Movie.ToListAsync());
+            movies = movies.Where(s => s.Title.Contains(searchString));
         }
 
+        if (!string.IsNullOrEmpty(movieGenre))
+        {
+            movies = movies.Where(x => x.Genre == movieGenre);
+        }
+
+        var movieGenreVM = new MovieGenreViewModel
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Movies = await movies.ToListAsync()
+        };
+
+        return View(movieGenreVM);
+    }
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
